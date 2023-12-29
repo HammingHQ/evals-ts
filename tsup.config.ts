eca56dc192932197f437ca2742b45f40691effff
Marius Buleandra <mbuleandra@gmail.com>
2023-12-29 13:13:54 -0800

    lint

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,20 +1,20 @@
-import type { Options } from 'tsup';
+import type { Options } from "tsup";
 
 const env = process.env.NODE_ENV;
 
-const isDev = env === 'development';
-const isProd = env === 'production';
+const isDev = env === "development";
+const isProd = env === "production";
 
 export const tsup: Options = {
   sourcemap: isProd,
   splitting: true,
   clean: true,
-  format: ['cjs', 'esm'],
-  entryPoints: ['src/index.ts'],
+  format: ["cjs", "esm"],
+  entryPoints: ["src/index.ts"],
   dts: true,
   minify: isProd,
-  target: 'es2021',
-  outDir: isProd ? 'dist' : 'lib',
+  target: "es2021",
+  outDir: isProd ? "dist" : "lib",
   watch: isDev,
-  entry: ['src/**/*.ts']
+  entry: ["src/**/*.ts"],
 };
